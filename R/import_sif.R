54d51ce6b0c9fef992edec8cb1edc9419aad1d9d
Nicola <casiraghinicola@gmail.com>
2019-03-17 17:53:32 +0100

    import_sif function as test

---
+++ R/import_sif.R
@@ -0,0 +1,18 @@
+#' Read and check the sample info file
+#'
+#' @param filesif A tab delimeted file without header where columns are:
+#' @return A list containing 2 data frames:
+#' [[1]] Table with unique germline samples (use for GSE distribution and pbem computation steps)
+#' [[2]] Table with only case samples having a matched control sample (use for call snvs step)
+
+import_sif <- function(filesif){
+  df  <-  read.delim(filesif,as.is=T,stringsAsFactors = F,header = F)
+  colnames(df) <- c("patient","plasma","plasma.bam","germline","germline.bam")
+  # remove NAs and keep only unique germline samples
+  df_ctrl = df[which(!is.na(df$germline.bam)),]
+  df_ctrl = unique(df_ctrl[,c(1,4,5)])
+  # remove NAs and keep only case samples having matched germline samples
+  nas = which(is.na(df$plasma.bam) | is.na(df$germline.bam))
+  if(length(nas)>0){df_cases = df_cases[-nas,]}
+  return(list(df_ctrl,df_cases))
+}
