57ce4426dcd07aded924fe1ee102702f703e13d2
Nicola <casiraghinicola@gmail.com>
2019-05-01 13:52:56 +0200

    get output summary index table for calls

---
+++ R/get_tabcalls_path.R
@@ -0,0 +1,15 @@
+#' get_tabcalls_path
+#'
+get_tabcalls_path <- function(TableSif,outdir,outdir.calls.name){
+  tabsnvs_index <- TableSif[,c(1,2,4)]
+  tabsnvs_index$tabcalls_f1 <- NA
+  tabsnvs_index$tabcalls_f2 <- NA
+  tabsnvs_index$tabcalls_f3 <- NA
+  for(i in 1:nrow(TableSif)){
+    xcs <- list.files(file.path(outdir,outdir.calls.name,tabsnvs_index$plasma[i]),recursive = F,full.names = T,pattern = "pmtab_")
+    tabsnvs_index$tabcalls_f1[i] <- grep(xcs, pattern = "pmtab_F1", value = T)
+    tabsnvs_index$tabcalls_f2[i] <- grep(xcs, pattern = "pmtab_F2", value = T)
+    tabsnvs_index$tabcalls_f3[i] <- grep(xcs, pattern = "pmtab_F3", value = T)
+  }
+  return( tabsnvs_index )
+}
