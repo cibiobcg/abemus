b88616b6eb61d1df5a48535d4bd3730b5feb64b2
Nicola <casiraghinicola@gmail.com>
2019-05-04 19:20:23 +0200

    functions for split pabs and pileups from pacbam by chromosome

---
+++ R/pileup_splitbychr.R
@@ -0,0 +1,8 @@
+#' pileup_splitbychr
+pileup_splitbychr <- function(id,CHR,pileup_file){
+  chrom = CHR[id]
+  filename = gsub(basename(pileup_file),pattern = ".pileup",replacement = paste0("_chr",gsub(chrom,pattern = "chr",replacement = ""),".pileup"))
+  cat(c("chr","pos","ref","A","C","G","T","af","cov","dbsnp\n"),sep = "\t",file = filename)
+  awk = paste0("/usr/bin/awk '{if ($1 == \"",chrom,"\") { print $0; }}' ",pileup_file," >> ",filename)
+  system(awk)
+}
