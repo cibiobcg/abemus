370c5853bbb7c207dd82237b71281a66a7dd0d56
Nicola <casiraghinicola@gmail.com>
2019-05-05 21:10:21 +0200

    get only one chrom per time and then trasfrom bed to each single position

--- R/bed2positions.R
+++ R/bed2positions.R
@@ -1,26 +1,37 @@
 #' bed2positions
 #' @param targetbed Genomic regions in the BED tab-delimited format.
+#' @param get_only_chromosomes return only the list of chromosomes in the BED file
+#' @param chrom_to_extract the chromosome name to extract as i.e. "chr1"
 #' @return PosByChrom A list of data.frames, each for each chromosome.
 #' @return chromosomes The chromosomes included in the BED file.
 #' @export
-bed2positions <- function(targetbed){
-  bed <- fread(input = targetbed,colClasses = list(character=1),data.table = F,stringsAsFactors = F,header = F)
-  bed$V2 <- bed$V2+1
-  chromosomes <- gsub(unique(bed$V1),pattern = "chr",replacement = "")
-  chromosomes <- sort(paste0("chr",chromosomes))
-  PosByChrom <- list()
+bed2positions <- function(targetbed,
+                          chrom_to_extract,
+                          get_only_chromosomes = F){
+  if(get_only_chromosomes){
+    bed <- fread(input = targetbed,colClasses = list(character=1),data.table = F,stringsAsFactors = F,header = F)
+    bed$V1 <- gsub(bed$V1,pattern = "chr",replacement = "")
+    bed$V1 <- paste0("chr",bed$V1)
+    chromosomes <- sort(unique(bed$V1))
 
-  unwrap <- function(x){
-    return(seq.int(from = x[2],to = x[3]))
+    return(list(chromosomes=chromosomes))
+
+  } else {
+    bed <- fread(input = targetbed,colClasses = list(character=1),data.table = F,stringsAsFactors = F,header = F)
+    bed$V1 <- gsub(bed$V1,pattern = "chr",replacement = "")
+    bed$V1 <- paste0("chr",bed$V1)
+    bed$V2 <- bed$V2+1
+    chromosomes <- sort(unique(bed$V1))
+
+    unwrap <- function(x){return(seq.int(from = x[2],to = x[3]))}
+
+    PosByChrom <- data.frame(chr=chr,
+                             pos=as.numeric(unlist( apply(bed_chrom, MARGIN=1,FUN = unwrap))),
+                             stringsAsFactors = F)
+    PosByChrom$group <- paste(PosByChrom$chr,PosByChrom$pos,sep=":")
+
+    return(list(chromosomes=chromosomes, PosByChrom=PosByChrom))
+
   }
-
-  for(chr in chromosomes){
-    bed_chrom <- bed[grep(bed$V1,pattern = chr),,drop=F]
-    PosByChrom[[chr]] <- data.frame(chr=chr,
-                                    pos=as.numeric(unlist( apply(bed_chrom, MARGIN=1,FUN = unwrap))),
-                                    stringsAsFactors = F)
-  }
-
-  return(list(chromosomes=chromosomes, PosByChrom=PosByChrom))
-
 }
+
