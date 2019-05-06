0e63c930e8c4d325b9dac8b63e72dcfe20033e23
Nicola <casiraghinicola@gmail.com>
2019-05-06 11:44:23 +0200

    apply_scaling_factor() function improved. automatically select the optimal R to be applied given mean coverage and target size.

--- R/apply_scaling_factor.R
+++ R/apply_scaling_factor.R
@@ -1,20 +1,72 @@
 #' apply_scaling_factor
 #' @export
-#' @param tabindex output from callsnvs()
+#' @param tabindex output from callsnvs(). (if use.optimal.R = TRUE , tabindex must contain the column "case_mean_coverage"; use get_case_mean_coverage())
 #' @param R scaling factor to be applied at filter.pbem_coverage value. default = 1
-apply_scaling_factor <- function(tabindex,R=1){
-  tabindex[,paste0("tabcalls_f3","_R",R)] <- NA
-  for(i in 1:nrow(tabindex)){
-    a <- fread(input = tabindex$tabcalls_f3[i],stringsAsFactors = F,data.table = F)
-    a$filter.pbem_coverage <- a$filter.pbem_coverage * R
-    a$pass.filter.pbem_coverage <- 0
-    a$pass.filter.pbem_coverage[which(a$af_case >= a$filter.pbem_coverage)] <- 1
-    a <- a[which(a$pass.filter.pbem_coverage==1),]
-    out.name <- gsub(basename(tabindex$tabcalls_f3[i]),pattern = "pmtab_F3_",replacement = paste0("pmtab_F3_R",R,"_"))
-    out.path <- gsub(tabindex$tabcalls_f3[i],pattern = basename(tabindex$tabcalls_f3[i]),replacement = out.name)
-    write.table(x = a,file = out.path,quote = F,sep = "\t",row.names = F,col.names = T)
-    tabindex[i,paste0("tabcalls_f3","_R",R)] <- out.path
+#' @param use.optimal.R Automatically use the best R given mean coverage of the case sample and size of targeted regions. default: FALSE
+#' @param target_size (if use.optimal.R = TRUE) target size in Mb of the BED file. Use get_target_size()
+apply_scaling_factor <- function(tabindex,
+                                 R = 1,
+                                 use.optimal.R = FALSE,
+                                 target_size = NA){
+
+  if(use.optimal.R){
+
+    #<check>
+    if(!"case_mean_coverage"%in%colnames(tabindex)){
+      cat(paste("[",Sys.time(),"]\tError. column 'case_mean_coverage' must be present in tabindex when use.optimal.R = TRUE","\n"))
+      stop()
+    }
+    if(is.na(target_size)){
+      cat(paste("[",Sys.time(),"]\tError. 'target_size' needed when use.optimal.R = TRUE","\n"))
+      stop()
+    }
+
+    tabindex[,paste0("tabcalls_f3","_optimalR")] <- NA
+    tabindex[,paste0("tabcalls_f3","_optimalR_used")] <- NA
+
+    for(i in 1:nrow(tabindex)){
+      a <- fread(input = tabindex$tabcalls_f3[i],stringsAsFactors = F,data.table = F)
+      # select best R for this case sample
+
+      sizes <- sort(unique(tab_optimal_R$target_Mbp))
+      closest_target_size <- sizes[which(abs(sizes-target_size)==min(abs(sizes-target_size)))]
+
+      bombanel <- tab_optimal_R[which(tab_optimal_R$target_Mbp == closest_target_size),]
+
+      covs <- bombanel$mean_coverage
+      closest_coverage <- covs[which(abs(covs-tabindex$case_mean_coverage[i])==min(abs(covs-tabindex$case_mean_coverage[i])))]
+
+      optR <- bombanel$scalingfactor[which(bombanel$mean_coverage==closest_coverage)]
+
+      a$filter.pbem_coverage <- a$filter.pbem_coverage * optR
+      a$pass.filter.pbem_coverage <- 0
+      a$pass.filter.pbem_coverage[which(a$af_case >= a$filter.pbem_coverage)] <- 1
+      a <- a[which(a$pass.filter.pbem_coverage==1),]
+      out.name <- gsub(basename(tabindex$tabcalls_f3[i]),pattern = "pmtab_F3_",replacement = paste0("pmtab_F3_optimalR_"))
+      out.path <- gsub(tabindex$tabcalls_f3[i],pattern = basename(tabindex$tabcalls_f3[i]),replacement = out.name)
+      write.table(x = a,file = out.path,quote = F,sep = "\t",row.names = F,col.names = T)
+      tabindex[i,paste0("tabcalls_f3","_optimalR")] <- out.path
+      tabindex[i,paste0("tabcalls_f3","_optimalR_used")] <- optR
+    }
+    cat(paste("[",Sys.time(),"]\talright.","\n"))
+    return(tabindex)
+
+  } else {
+
+    tabindex[,paste0("tabcalls_f3","_R",R)] <- NA
+    for(i in 1:nrow(tabindex)){
+      a <- fread(input = tabindex$tabcalls_f3[i],stringsAsFactors = F,data.table = F)
+      a$filter.pbem_coverage <- a$filter.pbem_coverage * R
+      a$pass.filter.pbem_coverage <- 0
+      a$pass.filter.pbem_coverage[which(a$af_case >= a$filter.pbem_coverage)] <- 1
+      a <- a[which(a$pass.filter.pbem_coverage==1),]
+      out.name <- gsub(basename(tabindex$tabcalls_f3[i]),pattern = "pmtab_F3_",replacement = paste0("pmtab_F3_R",R,"_"))
+      out.path <- gsub(tabindex$tabcalls_f3[i],pattern = basename(tabindex$tabcalls_f3[i]),replacement = out.name)
+      write.table(x = a,file = out.path,quote = F,sep = "\t",row.names = F,col.names = T)
+      tabindex[i,paste0("tabcalls_f3","_R",R)] <- out.path
+    }
+    cat(paste("[",Sys.time(),"]\talright.","\n"))
+    return(tabindex)
+
   }
-  cat(paste("[",Sys.time(),"]\talright.","\n"))
-  return(tabindex)
 }
