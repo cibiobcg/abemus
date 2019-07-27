ecdae392be044472e12ec284bd5104cdf7023eee
Nicola Casiraghi <nicolacasiraghi@b260-mb02-wl.local>
2019-07-27 14:20:55 +0200

    do not add 'chr'. chrom annotation in BED and pacbam data must be concordant

--- R/bed2positions.R
+++ R/bed2positions.R
@@ -1,7 +1,7 @@
 #' bed2positions
 #' @param targetbed Genomic regions in the BED tab-delimited format.
 #' @param get_only_chromosomes return only the list of chromosomes in the BED file. default: FALSE
-#' @param chrom_to_extract the chromosome name to extract as i.e. "chr1"
+#' @param chrom_to_extract the chromosome name to extract as i.e. "chr1" or "1" accordingly with BED file annotation
 #' @return PosByChrom A list of data.frames, each for each chromosome.
 #' @return chromosomes The chromosomes included in the BED file.
 #' @export
@@ -10,16 +10,12 @@ bed2positions <- function(targetbed,
                           get_only_chromosomes = F){
   if(get_only_chromosomes){
     bed <- fread(input = targetbed,colClasses = list(character=1),data.table = F,stringsAsFactors = F,header = F)
-    bed$V1 <- gsub(bed$V1,pattern = "chr",replacement = "")
-    bed$V1 <- paste0("chr",bed$V1)
     chromosomes <- sort(unique(bed$V1))
 
     return(list(chromosomes=chromosomes))
 
   } else {
     bed <- fread(input = targetbed,colClasses = list(character=1),data.table = F,stringsAsFactors = F,header = F)
-    bed$V1 <- gsub(bed$V1,pattern = "chr",replacement = "")
-    bed$V1 <- paste0("chr",bed$V1)
     bed$V2 <- bed$V2+1
     chromosomes <- sort(unique(bed$V1))
 
