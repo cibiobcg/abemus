73e8587e836c5a2f3d931628dd03e0af484b0801
ncasiraghi <nicolacasiraghi@b260-mb02-wl.local>
2019-07-27 16:11:16 +0200

    gsub chrom name

--- R/get_germlineset.R
+++ R/get_germlineset.R
@@ -7,6 +7,7 @@
 # @return A character string with the path to germline pileups
 get_germlineset <- function(sifgerm,pacbamfolder_bychrom,chrom){
   germlineset = c()
+  chrom <- gsub(chrom,pattern = 'chr',replacement = '')
   for(id in 1:nrow(sifgerm)){
     thisSample=sifgerm[id,]
     name = gsub(basename(thisSample$germline.bam),pattern = ".bam",replacement = "")
