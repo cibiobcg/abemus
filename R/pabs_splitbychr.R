31ba263cad7d9a7b7920e2b870f21f3b089bc793
ncasiraghi <casiraghinicola@gmail.com>
2019-11-11 19:19:30 +0100

    avoid awk

--- R/pabs_splitbychr.R
+++
@@ -1,7 +0,0 @@
-pabs_splitbychr <- function(id,CHR,snvs_file){
-  chrom = CHR[id]
-  filename = gsub(basename(snvs_file),pattern = ".pabs",replacement = paste0("_chr",gsub(chrom,pattern = "chr",replacement = ""),".pabs"))
-  cat(c("chr","pos","ref","alt","A","C","G","T","af","cov","Ars","Crs","Grs","Trs","dbsnp\n"),sep = "\t",file = filename)
-  awk = paste0("/usr/bin/awk '{if ($1 == \"",chrom,"\") { print $0; }}' ",snvs_file," >> ",filename)
-  system(awk)
-}
