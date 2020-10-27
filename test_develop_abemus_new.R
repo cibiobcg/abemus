db4d92b04355dda08d1c52b8f09f5404ea144b07
Nicola <casiraghinicola@gmail.com>
2020-10-27 09:29:13 +0100

    main functions added

--- test_develop_abemus_new.R
+++ test_develop_abemus_new.R
@@ -4,21 +4,52 @@ devtools::install_github("cibiobcg/abemus", build_vignettes = T,ref = 'develop')
 library(data.table)
 library(parallel)
 library(Matrix)
+library(abemus)
 
 outdir <-  "/BCGLAB/ncasiraghi/abemus_test_develop_branch"
-sample.info.file <- "/BCGLAB/ncasiraghi/abemus_test_develop_branch/sample_info.txt"
+sample.info.file <- "/BCGLAB/ncasiraghi/abemus_test_develop_branch/sample_info_toy.txt"
 targetbed <- "/BCGLAB/ncasiraghi/abemus_test_develop_branch/TST170_DNA_target.nochr.merged.nochromY.bed"
 pacbamfolder_bychrom <- "/BCGLAB/ncasiraghi/abemus_test_develop_branch/bychrom/"
+
+import_sif <- function(main_sif){
+  df  <-  read.delim(main_sif,as.is=TRUE,stringsAsFactors = FALSE,header = FALSE)
+  colnames(df) <- c("patient","plasma","plasma.bam","germline","germline.bam")
+  # remove NAs and keep only unique germline samples
+  df_ctrl <- df[which(!is.na(df$germline.bam)),,drop=FALSE]
+  df_ctrl <- unique(df_ctrl[,c(4,5)])
+  # remove NAs and keep only case samples having matched germline samples
+  df_cases <- df[which(!is.na(df$plasma.bam)),,drop=FALSE]
+  return(list(df_ctrl=df_ctrl,df_cases=df_cases))
+}
 
 sif <- import_sif(sample.info.file)
 
 bed <- bed2positions(targetbed = targetbed,get_only_chromosomes = TRUE)
 
+get_germlineset <- function(sifgerm,pacbamfolder_bychrom,chrom){
+  germlineset = c()
+  chrom <- gsub(chrom,pattern = 'chr',replacement = '')
+  for(id in seq_len(nrow(sifgerm))){
+    thisSample=sifgerm[id,]
+    name = gsub(basename(thisSample$germline.bam),pattern = ".bam",replacement = "")
+    thisPileup.file = list.files(file.path(pacbamfolder_bychrom,name,"pileup"),full.names = TRUE,pattern = paste0("_chr",chrom,"\\.pileup$"))
+    germlineset = c(germlineset,thisPileup.file)
+  }
+  germlineset <- unique(germlineset)
+  return(germlineset)
+}
+
 getInfo <- function(chrom,sif,pacbamfolder_bychrom,select){
   germlineset <- get_germlineset(sifgerm = sif$df_ctrl,
                                  pacbamfolder_bychrom = pacbamfolder_bychrom,
                                  chrom = chrom)
-  df <- fread(germlineset[1],sep = '\t',stringsAsFactors = FALSE,skip = 1,select = select,verbose = FALSE,data.table=FALSE, nThread = 4)
+  df <- fread(germlineset[1],
+              sep = '\t',
+              stringsAsFactors = FALSE,
+              skip = 1,select = select,
+              verbose = FALSE,
+              data.table=FALSE,
+              nThread = 4)
   return(as.vector(unlist(df)))
 }
 
@@ -42,23 +73,83 @@ mat_cov <- lapply(bed$chromosomes,pileup2mat,sif,pacbamfolder_bychrom,select=9)
 
 dict <- 4:7
 names(dict) <- c('A','C','G','T')
-mat <- list()
+mat_cov_base <- list()
 for(base in names(dict)){
-  mat[[base]] <- lapply(bed$chromosomes,pileup2mat,sif,pacbamfolder_bychrom,select=as.integer(dict[base]),sparse=TRUE)
+  mat_cov_base[[base]] <- lapply(bed$chromosomes,pileup2mat,sif,pacbamfolder_bychrom,select=as.integer(dict[base]),sparse=TRUE)
 }
 
-# compute pbem
+# [ compute pbem ]
+# coverage_binning = 50
+# af_max_to_compute_pbem = 0.2
+# coverage_min_to_compute_pbem = 10
+# mc.cores = 1
+# n_pos_af_th = 0.2
+# step = 5000
 
-# select correct rows!
+chrom_pbem <- function(i,mat_cov,mat_cov_base,ref){
 
-num <- rowSums((mat[['A']][[6]] + mat[['C']][[6]] + mat[['G']][[6]]))
-den <- rowSums(mat_cov[[6]])
+  x <- rep(NA,length(ref[[i]]))
 
-pbem <- num/den
+  den <- rowSums(mat_cov[[i]],na.rm = TRUE)
 
-hist(pbem,100)
+  # ref A
+  idx <- which(ref[[i]] == 'A')
+  num <- rowSums(mat_cov_base[['C']][[i]][idx,] + mat_cov_base[['G']][[i]][idx,] + mat_cov_base[['T']][[i]][idx,],na.rm = TRUE)
+  x[idx] <- num/den[idx]
 
-# compute af threshold
+  # ref C
+  idx <- which(ref[[i]] == 'C')
+  num <- rowSums(mat_cov_base[['A']][[i]][idx,] + mat_cov_base[['G']][[i]][idx,] + mat_cov_base[['T']][[i]][idx,],na.rm = TRUE)
+  x[idx] <- num/den[idx]
+
+  # ref G
+  idx <- which(ref[[i]] == 'G')
+  num <- rowSums(mat_cov_base[['A']][[i]][idx,] + mat_cov_base[['C']][[i]][idx,] + mat_cov_base[['T']][[i]][idx,],na.rm = TRUE)
+  x[idx] <- num/den[idx]
+
+  # ref T
+  idx <- which(ref[[i]] == 'T')
+  num <- rowSums(mat_cov_base[['A']][[i]][idx,] + mat_cov_base[['C']][[i]][idx,] + mat_cov_base[['G']][[i]][idx,],na.rm = TRUE)
+  x[idx] <- num/den[idx]
+
+  return(x)
+}
+
+pbem_by_chrom <- lapply(seq_len(length(ref)),chrom_pbem,mat_cov,mat_cov_base,ref)
+
+# [ compute af threshold ] all chroms together
+# af_max_to_compute_thresholds = 0.2
+# coverage_min_to_compute_thresholds = 10
+# coverage_binning = 50,
+# probs = seq(0.9,1,0.0001)){
+
+spec <- 0.995
+max.vaf <- 0.2
+min.cov <- 10
+bin.cov <- 50
+
+# roll over chromosomes [!]
+cov <- as.vector(mat_cov[[i]])
+cov[which(cov < min.cov)] <- NA
+
+vaf <- as.vector(mat_vaf[[i]])
+vaf[which(vaf > max.vaf)] <- NA
+
+# vaf and cov must be genome wide [!]
+
+covbin <- cut(cov,
+              breaks=seq(min.cov,max(cov,na.rm = TRUE),by=bin.cov),
+              include.lowest=TRUE)
+barplot(table(covbin),las=2,cex.names = 0.4)
+
+for(bin in levels(covbin)){
+  message(bin)
+  m <- vaf[which(covbin == bin)]
+  quantile(m,probs = spec)
+}
+
+head(vaf)
+head(covbin)
 
 
 
@@ -67,25 +158,11 @@ hist(pbem,100)
 
 
 
-outdir.bperr.name = "BaseErrorModel"
-coverage_binning = 50
-af_max_to_compute_thresholds = 0.2
-coverage_min_to_compute_thresholds = 10
-af_max_to_compute_pbem = 0.2
-coverage_min_to_compute_pbem = 10
-n_pos_af_th = 0.2
-mc.cores = 1
-step = 5000
 
 
 
-sif <- read.delim(file = sample.info.file,header = F)
 
-sif$V5 <- file.path('/BCGLAB/ncasiraghi/abemus_test_develop_branch/bychrom/')
 
-bed <- read.delim(file = targetbed,header = F)
 
-head(bed)
 
-bed <- bed[which(bed$V1 != 'Y'),]
-write.table(bed,file = "/BCGLAB/ncasiraghi/abemus_test_develop_branch/TST170_DNA_target.nochr.merged.nochromY.bed",quote = F,sep = '\t',col.names = F,row.names = F)
+
